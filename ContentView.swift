0e75dcd3680f57e7017b404ba5e3e0cacc5acc03
Sebastian <sebastiangarciapachano@gmail.com>
2025-05-26 22:37:35 +0200

    first commit

---
+++ ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RickMortyApi
+//
+//  Created by Isabella Garcia on 26/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RickMortyApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyApiApp.swift
+//  RickMortyApi
+//
+//  Created by Isabella Garcia on 26/5/25.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
