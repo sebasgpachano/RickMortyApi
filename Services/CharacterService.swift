db50b9325b5f35ed7b3a026c5807f933d976fe05
Sebastian <sebastiangarciapachano@gmail.com>
2025-05-27 22:55:11 +0200

    added function for character details view

--- Services/CharacterService.swift
+++ Services/CharacterService.swift
@@ -8,9 +8,13 @@
 import Foundation
 
 struct CharacterService {
-    func fetchCharacters(completion: @escaping (Result<[Character], Error>) -> Void) {
-        let url = URL(string: "https://rickandmortyapi.com/api/character")!
-        
+    func fetchCharacters(page: Int = 1, completion: @escaping (Result<CharacterResponse, Error>) -> Void) {
+        let urlString = "https://rickandmortyapi.com/api/character?page=\(page)"
+        guard let url = URL(string: urlString) else {
+            completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "URL inválida"])))
+            return
+        }
+
         URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error {
                 completion(.failure(error))
@@ -24,7 +28,34 @@ struct CharacterService {
 
             do {
                 let response = try JSONDecoder().decode(CharacterResponse.self, from: data)
-                completion(.success(response.results))
+                completion(.success(response))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+    
+    func fetchCharacterDetail(id: Int, completion: @escaping (Result<Character, Error>) -> Void) {
+        let urlString = "https://rickandmortyapi.com/api/character/\(id)"
+        guard let url = URL(string: urlString) else {
+            completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "URL inválida"])))
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+
+            guard let data = data else {
+                completion(.failure(NSError(domain: "", code: -1, userInfo: [NSLocalizedDescriptionKey: "No data"])))
+                return
+            }
+
+            do {
+                let character = try JSONDecoder().decode(Character.self, from: data)
+                completion(.success(character))
             } catch {
                 completion(.failure(error))
             }
