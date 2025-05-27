7793af7d2247b890ed6941fcee669d82215ff296
Sebastian <sebastiangarciapachano@gmail.com>
2025-05-27 10:33:39 +0200

    character viewmodel

---
+++ ViewModels/CharacterViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  CharacterViewModel.swift
+//  RickMortyApi
+
+import Foundation
+
+@MainActor
+final class CharacterListViewModel: ObservableObject {
+    @Published var characters: [Character] = []
+    @Published var isLoading: Bool = false
+    @Published var errorMessage: String? = nil
+
+    private let characterService: CharacterServiceProtocol
+
+    init(characterService: CharacterServiceProtocol = CharacterService()) {
+        self.characterService = characterService
+    }
+
+    func fetchCharacters() {
+        isLoading = true
+        errorMessage = nil
+
+        characterService.fetchCharacters { [weak self] result in
+            guard let self = self else { return }
+
+            Task {
+                self.isLoading = false
+                switch result {
+                case .success(let characters):
+                    self.characters = characters
+                case .failure(let error):
+                    self.errorMessage = error.localizedDescription
+                }
+            }
+        }
+    }
+}
